23ead24ac62e2cc7ca64c4a95c386aa78023b305
Patrick O'Neill <synapse.and.syntax@gmail.com>
2013-02-17 14:11:53 -0500

    fixed ecoli home file

--- utils.py
+++ utils.py
@@ -495,7 +495,7 @@ print "loaded utils"
 
 def get_ecoli_genome(at_lab=True):
     lab_file = "/home/poneill/ecoli/NC_000913.fna"
-    home_file = "/home/pat/Dropbox/entropy/NC_000913.fna"
+    home_file = "/home/pat/ecoli/NC_000913.fna"
     with open(lab_file if at_lab else home_file) as f:
         genome = "".join([line.strip() for line in f.readlines()[1:]])
     return "".join(g for g in genome if g in "ATGC") # contains other iupac symbols
